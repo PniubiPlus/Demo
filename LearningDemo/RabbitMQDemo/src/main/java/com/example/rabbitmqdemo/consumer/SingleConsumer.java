3bdaaca16b602014a51f4dc26456eda8181b0fb9
P <13903833145@163.com>
2026-08-08 10:19:28 +0800

    补充

--- LearningDemo/RabbitMQDemo/src/main/java/com/example/rabbitmqdemo/consumer/SingleConsumer.java
+++ LearningDemo/RabbitMQDemo/src/main/java/com/example/rabbitmqdemo/consumer/SingleConsumer.java
@@ -1,13 +1,19 @@
 package com.example.rabbitmqdemo.consumer;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
+@RabbitListener(bindings = @QueueBinding(
+        value = @Queue(name = "",durable = "true"),
+        exchange = @Exchange(name = "exchangeName",type = ExchangeTypes.DIRECT,durable = "true"),
+        key =
+))
 public class SingleConsumer {
 
-    // 监听一对一队列：一条消息只会被一个消费者处理
-    @RabbitListener(queues = "${app.rabbitmq.queue.single}")
+    // 监听一对一队列：一条消息只会被一个消费者处理           midengxing
+    @RabbitHandler
     public void receive(String message) {
         System.out.println("【一对一 SingleConsumer】收到消息: " + message);
     }
